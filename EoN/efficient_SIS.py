95c5d46cd466c3453e048b9fdebb46a89a5021d1
Guillaume St-Onge <guillaume.st-onge.4@ulaval.ca>
2018-12-07 12:33:15 -0500

    created function returning efficient methods to get transmission delay and the associated arguments.

--- EoN/efficient_SIS.py
+++ EoN/efficient_SIS.py
@@ -6,7 +6,8 @@ using the event-driven approach.
 
 import networkx
 import numpy as np
-from random import random
+from numpy.random import poisson
+from numpy.random import random
 
 __author__ = "Guillaume St-Onge"
 __copyright__ = "Copyright (c) 2018 Guillaume St-Onge"
@@ -48,11 +49,40 @@ class NeighborSampler:
         return self.neighbor_label_list_dict[node][index]
 
 
-def _get_efficient_trans_and_rec_fxn_(G, cumulative_trans_fxn,
-                                      inverse_cumulative_trans_fxn,
-                                      inverse_cumulative_rec_fxn):
+def _get_efficient_trans_and_rec_fxn_SIS_(G, status, rec_time, time,
+                                          cumulative_trans_fxn,
+                                          inverse_cumulative_trans_fxn,
+                                          inverse_cumulative_rec_fxn):
     """Returns efficient methods to determine the transmission to neighbors
     and arguments to be used along with fast_nonMarkov_SIS.
     """
-    pass
 
+    args = tuple([NeighborSampler(G), status, rec_time, time])
+
+    def trans_and_rec(node, susceptible_neighbors, neighbor_sampler, status,
+                     rec_time, time):
+        #first get recuperation delay
+        rec_delay = inverse_cumulative_rec_fxn(-np.log(random()))
+        cumulative_infection = cumulative_trans_fxn(rec_delay)
+        #get the number of transmission attempts
+        transmission_attempts = poisson(
+            neighbor_sampler.get_total_weight(node)*cumulative_infection)
+        #create a dictionary of list of transmission delay for each neighbor
+        trans_delay_dict = dict()
+        for i in range(transmission_attempts):
+            neighbor = neighbor_sampler.get_random_neighbor(node)
+            #verify first if transmission is plausible
+            if not (status[neighbor] == 'I' and
+                    rec_time[neighbor] > time + rec_delay):
+                trans_delay = inverse_cumulative_trans_fxn(
+                    random()*cumulative_infection)
+                if neighbor in trans_delay_dict:
+                    trans_delay_dict[neighbor].append(trans_delay)
+                else:
+                    trans_delay_dict[neighbor] = [trans_delay]
+                #Note : a second verification if the transmission is plausible
+                #for this trans_delay is performed in the main code
+                #in the function _process_trans_SIS_nonMarkov_
+        return trans_delay_dict, rec_delay
+
+    return trans_and_rec, args
